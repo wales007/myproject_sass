76ebc04a14546be7d88f955205bfa25d1a012af1
olawale olaniyan <incspired@gmail.com>
2016-10-17 13:19:43 +0000

    Fixed errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,5 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password,
 :password_confirmation) }
-  end
+   end
 end
